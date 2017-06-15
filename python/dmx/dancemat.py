243d4d5dab4eff133cd288b0618f6f25d2ef0e67
Richard <richardhayes777@gmail.com>
2017-06-15 19:59:39 +0100

    created basic code to get connection. reading permissions problem.

---
+++ python/dmx/dancemat.py
@@ -0,0 +1,22 @@
+import sys
+import usb.core
+import usb.util
+
+
+class DanceMat:
+    def __init__(self, product_id=0x0011, vendor_id=0x0079):
+        self.dev = usb.core.find(idVendor=vendor_id, idProduct=product_id)
+
+        interface = 0
+        self.endpoint = self.dev[0][(0, 0)][0]
+
+        # if the OS kernel already claimed the device, which is most likely true
+        # thanks to http://stackoverflow.com/questions/8218683/pyusb-cannot-set-configuration
+        if self.dev.is_kernel_driver_active(interface) is True:
+            self.dev.detach_kernel_driver(interface)
+            usb.util.claim_interface(self.dev, interface)
+
+    def read(self):
+        self.dev.read(self.endpoint.bEndpointAddress, self.endpoint.wMaxPacketSize)
+
+
