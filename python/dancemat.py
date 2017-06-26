acd3bc54dcd8558fb918e8a9f8fe17e63cab0ef2
Richard <richardhayes777@gmail.com>
2017-06-26 21:17:08 +0100

    using button state dictionary to fix duplicate note issue

--- python/dancemat.py
+++ python/dancemat.py
@@ -28,23 +28,16 @@ class DanceMat:
     def __init__(self, number=0):
         self.joystick = pygame.joystick.Joystick(number)
         self.joystick.init()
-        self.listener_dict = {}
         self.button_listener = None
 
     # Read data and alert listeners
     def read(self):
         for event in pygame.event.get():
-            for n in range(0, self.joystick.get_numbuttons()):
-                is_on = self.joystick.get_button(n) == 1
-                button = Button.all[n]
-                if button in self.listener_dict:
-                    self.listener_dict[n](is_on)
-                if self.button_listener is not None:
-                    self.button_listener(button, is_on)
-
-    # Adds a listener to listen for a specific button being pressed or depressed
-    def add_listener_to_button(self, button, listener):
-        self.listener_dict[button] = listener
+            print "EVENT"
+            if self.button_listener is not None:
+                button_dict = {Button.all[n]: self.joystick.get_button(n) == 1 for n in
+                               range(0, self.joystick.get_numbuttons())}
+                self.button_listener(button_dict)
 
     # Sets a listener that is alerted to any button press or depress
     def set_button_listener(self, button_listener):
--- python/dancestrument.py
+++ python/dancestrument.py
@@ -23,38 +23,40 @@ playing_notes = {}
 
 
 # Function to listen for changes to button state
-def listener(button, is_pressed):
-    # Check if button is in the dictionary
-    if button in position_dict:
-        # See if a note is already playing for that button
-        if button in playing_notes:
-            # If there is a note playing, grab that
-            note = playing_notes[button]
-        else:
-            # Otherwise, grab the position in the scale associated with the button...
-            position = position_dict[button]
-            # ...and create a new jazzy seventh chord for that position (this is gonna be in the scale)
-            # note = scale.chord(position, intervals=music.Chord.seventh_octave)
-            note = scale.chord(position)
-        # Check if the button is pressed down
-        if is_pressed:
-            print "{} pressed".format(button)
-            # If this is, play the note
-            instrument.play(note)
-            # Make sure the note is in the playing_notes dictionary so we can get it later
-            playing_notes[button] = note
-        else:
-            # Otherwise, stop playing the note...
-            instrument.stop(note)
-            # And if the note is in the dictionary, delete it
+def listener(status_dict):
+    for button in status_dict.keys():
+        is_pressed = status_dict[button]
+        # Check if button is in the dictionary
+        if button in position_dict:
+            # See if a note is already playing for that button
             if button in playing_notes:
-                del playing_notes[button]
-    # If the button wasn't in the dictionary, it's because it's select or start
-    elif is_pressed:
-        # Stop the instruments
-        instrument.stop_all()
-        # Change octave, going down for select and up for start
-        scale.change_octave(-1 if button == dancemat.Button.select else 1)
+                # If there is a note playing, grab that
+                note = playing_notes[button]
+            else:
+                # Otherwise, grab the position in the scale associated with the button...
+                position = position_dict[button]
+                # ...and create a new jazzy seventh chord for that position (this is gonna be in the scale)
+                # note = scale.chord(position, intervals=music.Chord.seventh_octave)
+                note = scale.chord(position)
+            # Check if the button is pressed down
+            if is_pressed and note not in playing_notes:
+                print "{} pressed".format(button)
+                # If this is, play the note
+                instrument.play(note)
+                # Make sure the note is in the playing_notes dictionary so we can get it later
+                playing_notes[button] = note
+            else:
+                # Otherwise, stop playing the note...
+                instrument.stop(note)
+                # And if the note is in the dictionary, delete it
+                if button in playing_notes:
+                    del playing_notes[button]
+        # If the button wasn't in the dictionary, it's because it's select or start
+        elif is_pressed:
+            # Stop the instruments
+            instrument.stop_all()
+            # Change octave, going down for select and up for start
+            scale.change_octave(-1 if button == dancemat.Button.select else 1)
     # This is called to make changes to the instrument have effect (i.e. it sends midi messages)
     instrument.update()
 
