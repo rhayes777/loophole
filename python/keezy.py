de9a3d45ae645a3a7d1bd1eb38a7a1229c6a46ed
Richard <richardhayes777@gmail.com>
2017-07-03 21:47:39 +0100

    bug fix that doesnt make things work

--- python/keezy.py
+++ python/keezy.py
@@ -16,15 +16,17 @@ position_dict = {dancemat.Button.triangle: 0,
                  # dancemat.Button.up: 6,
                  # dancemat.Button.circle: 7}
 
+tracks = [sample.WHITE_atmos, sample.WHITE_guitar, sample.WHITE_percnsub, sample.WHITE_sitar]
+
 
 # Function to listen for changes to button state
 def listener(status_dict):
     for button in status_dict.keys():
         is_pressed = status_dict[button]
         # Check if button is in the dictionary
-        if button in position_dict:
+        if button in position_dict and is_pressed:
             number = position_dict[button]
-            sample.queues[number].put(1 if is_pressed else 0)
+            sample.loop_wav_on_new_thread(tracks[number], no_of_times_to_loop=1)
 
 # Attach that listener function to the dancemat
 mat.set_button_listener(listener)
