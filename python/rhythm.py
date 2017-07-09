c35fc9abd4460668f79f1925c1793872f184a4e9
Richard <richardhayes777@gmail.com>
2017-07-09 18:33:27 +0100

    insane bug

--- python/rhythm.py
+++ python/rhythm.py
@@ -10,18 +10,14 @@ class SongClock:
     def __init__(self, bpm):
         self.bpm = bpm
         self.step = self.bpm / 60
+        self.eigth_step = self.step / 8
         self.functions = []
         self.thread = Thread(target=self.run, args=())
 
     def run(self):
-        while 1:
-            time.sleep(self.step)
-            print "running functions"
-            for function in self.functions:
-                function()
-
-    def start(self):
-        threading.Timer(self.step, self.run).start()
+        threading.Timer(self.eigth_step, self.run).start()
+        for function in self.functions:
+            function()
 
     def add_function(self, function):
         self.functions.append(function)
