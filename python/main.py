02fc042f9b7595c3f356a8b5edbc9cf3a94fe5fa
Richard <richardhayes777@gmail.com>
2017-07-18 09:18:36 +0100

    bend experiment

--- python/main.py
+++ python/main.py
@@ -20,13 +20,35 @@ def add_note(note, length=1.0):
     clock.add_action(play, stop, length=length)
 
 
-add_note(scale.note(0))
-add_note(scale.note(3))
-add_note(scale.note(2))
-add_note(scale.note(5))
-add_note(scale.note(0), length=0.5)
-add_note(scale.note(3), length=0.5)
-add_note(scale.note(2), length=0.5)
-add_note(scale.note(5), length=0.5)
+instrument.play(scale.note(0))
+
+
+def add_bend(value):
+    def bend():
+        instrument.midi_output.write_short(0xb0, 77, value)
+
+    clock.add_action(bend)
+
+add_bend(0)
+add_bend(1)
+add_bend(2)
+add_bend(4)
+add_bend(16)
+add_bend(50)
+add_bend(100)
+add_bend(200)
+add_bend(300)
+add_bend(400)
+add_bend(1000)
+add_bend(2000)
+
+# add_note(scale.note(0))
+# add_note(scale.note(3))
+# add_note(scale.note(2))
+# add_note(scale.note(5))
+# add_note(scale.note(0), length=0.5)
+# add_note(scale.note(3), length=0.5)
+# add_note(scale.note(2), length=0.5)
+# add_note(scale.note(5), length=0.5)
 
 clock.start()
