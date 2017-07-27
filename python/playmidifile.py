4acd3522f80f2de444c915046e9c3797123da84e
Richard <richardhayes777@gmail.com>
2017-07-27 21:03:07 +0100

    commands

--- python/playmidifile.py
+++ python/playmidifile.py
@@ -7,17 +7,41 @@ instrument = music.MidiInstrument()
 queue = Queue()
 
 
+# noinspection PyClassHasNoInit
+class Command:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    pitch_bend = "pitch_bend"
+    include_channel = "include_channel"
+    disclude_channel = "disclude_channel"
+
+
 def play_midi_file(name='big-blue.mid'):
     # noinspection PyUnresolvedReferences
     port = mido.open_output()
 
+    discluded_channels = set()
+
     mid = mido.MidiFile(name)
     for msg in mid.play():
         if not queue.empty():
-            instrument.pitch_bend(queue.get())
-        port.send(msg)
+            command = queue.get()
+            if command.name == Command.pitch_bend:
+                instrument.pitch_bend(command.value)
+            elif command.name == Command.disclude_channel:
+                discluded_channels.add(command.value)
+            elif command.name == Command.include_channel:
+                discluded_channels.remove(command.value)
+        if msg.channel not in discluded_channels:
+            port.send(msg)
 
 
 def play_midi_file_on_new_thread(name='big-blue.mid'):
     t = Thread(target=play_midi_file, args=(name,))
     t.start()
+
+
+def send_command(name, value):
+    queue.put(Command(name, value))
