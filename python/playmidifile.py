e3c2e4963d15da1bdbd60533f5a3a563ed2c6808
Richard <richardhayes777@gmail.com>
2017-07-27 20:38:35 +0100

    thread playing with queue example

--- python/playmidifile.py
+++ python/playmidifile.py
@@ -1,4 +1,10 @@
 import mido
+from Queue import Queue
+import music
+from threading import Thread
+
+instrument = music.MidiInstrument()
+queue = Queue()
 
 
 def play_midi_file(name='big-blue.mid'):
@@ -7,4 +13,10 @@ def play_midi_file(name='big-blue.mid'):
 
     mid = mido.MidiFile(name)
     for msg in mid.play():
+        if not queue.empty():
+            instrument.pitch_bend(queue.get())
         port.send(msg)
+
+
+def play_midi_file_on_new_thread():
+    Thread(target=play_midi_file).start()
