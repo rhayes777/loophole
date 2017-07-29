048af53cbec8ec4747814fe1f16964654aa406e1
Richard <richardhayes777@gmail.com>
2017-07-29 17:29:13 +0100

    improved track setup and track stopping

--- python/midi_player.py
+++ python/midi_player.py
@@ -3,6 +3,7 @@ from Queue import Queue
 import music
 from threading import Thread
 import logging
+import signal
 
 instrument = music.MidiInstrument()
 
@@ -18,37 +19,49 @@ class Command:
 
 
 class Track:
-    def __init__(self, track_name):
+    def __init__(self, track_name="bicycle-ride.mid", is_looping=False, included_channels=()):
         self.track_name = track_name
         self.queue = Queue()
+        self.is_stopping = False
+        self.is_looping = is_looping
+        self.included_channels = included_channels
+        signal.signal(signal.SIGINT, self.stop)
 
     def play_midi_file(self):
+        self.is_stopping = False
         # noinspection PyUnresolvedReferences
         port = mido.open_output()
 
-        included_channels = []
-
         mid = mido.MidiFile("media/{}".format(self.track_name))
         for msg in mid.play():
+            if self.is_stopping:
+                break
             if not self.queue.empty():
                 command = self.queue.get()
                 if isinstance(command, Command):
                     if command.name == Command.pitch_bend:
                         instrument.pitch_bend(command.value)
                     elif command.name == Command.set_included_channels:
-                        included_channels = command.value
+                        self.included_channels = command.value
             try:
-                if msg.channel in included_channels:
+                if msg.channel in self.included_channels:
                     port.send(msg)
             except AttributeError as e:
                 logging.exception(e)
+        if self.is_looping and not self.is_stopping:
+            self.play_midi_file()
 
     def start(self):
         t = Thread(target=self.play_midi_file)
         t.start()
 
+    def stop(self):
+        self.is_stopping = True
+
     def send_command(self, name, value):
         self.queue.put(Command(name, value))
 
     def set_included_channels(self, channels):
+        if isinstance(channels, int):
+            channels = [channels]
         self.send_command(Command.set_included_channels, channels)
