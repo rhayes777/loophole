d2d8611c3935620c8611321fd9ee3f615f1dcd6b
Richard <richardhayes777@gmail.com>
2017-07-29 17:59:25 +0100

    channel class giving greater customisation of individual channels

--- python/midi_player.py
+++ python/midi_player.py
@@ -7,36 +7,64 @@ import signal
 
 try:
     instrument = music.MidiInstrument()
-except Exception as e:
-    logging.exception(e)
+    # noinspection PyUnresolvedReferences
+    synth_port = mido.open_output()
+except Exception:
+    pass
 
 
-# noinspection PyClassHasNoInit
 class Command:
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
     pitch_bend = "pitch_bend"
-    set_included_channels = "set_included_channels"
+    volume = "volume"
+
+
+class Channel:
+    def __init__(self, number, port=synth_port, volume=1.0):
+        self.number = number
+        self.port = port
+        self.volume = volume
+        self.queue = Queue()
+
+    def send_message(self, msg):
+        try:
+            if not self.queue.empty():
+                command = self.queue.get()
+                if command.name == Command.volume:
+                    self.volume = command.value
+            msg.velocity = int(self.volume * msg.velocity)
+            self.port.send(msg)
+        except AttributeError:
+            pass
+
+    def set_volume(self, volume):
+        self.queue.put(Command(Command.volume, volume))
 
 
 class Track:
-    def __init__(self, track_name="bicycle-ride.mid", is_looping=False, included_channels=()):
+    def __init__(self, track_name="bicycle-ride.mid", is_looping=False):
         self.track_name = track_name
         self.queue = Queue()
         self.is_stopping = False
         self.is_looping = is_looping
-        self.included_channels = included_channels
+        self.mid = mido.MidiFile("media/{}".format(self.track_name))
         signal.signal(signal.SIGINT, self.stop)
+        channel_numbers = set()
+        for track in self.mid.tracks:
+            for msg in track:
+                try:
+                    channel_numbers.add(msg.channel)
+                except AttributeError:
+                    pass
+        self.channels = map(Channel, sorted(list(channel_numbers)))
 
     def play_midi_file(self):
         self.is_stopping = False
-        # noinspection PyUnresolvedReferences
-        port = mido.open_output()
 
-        mid = mido.MidiFile("media/{}".format(self.track_name))
-        for msg in mid.play():
+        for msg in self.mid.play():
             if self.is_stopping:
                 break
             if not self.queue.empty():
@@ -44,12 +72,11 @@ class Track:
                 if isinstance(command, Command):
                     if command.name == Command.pitch_bend:
                         instrument.pitch_bend(command.value)
-                    elif command.name == Command.set_included_channels:
-                        self.included_channels = command.value
             try:
-                if msg.channel in self.included_channels:
-                    port.send(msg)
+                self.channels[msg.channel].send_message(msg)
             except AttributeError as e:
+                logging.exception(e)
+            except IndexError as e:
                 logging.exception(e)
         if self.is_looping and not self.is_stopping:
             self.play_midi_file()
@@ -63,8 +90,3 @@ class Track:
 
     def send_command(self, name, value):
         self.queue.put(Command(name, value))
-
-    def set_included_channels(self, channels):
-        if isinstance(channels, int):
-            channels = [channels]
-        self.send_command(Command.set_included_channels, channels)
