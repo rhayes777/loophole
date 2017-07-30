e8bde988ed8240ed64953489527089554b756b63
stomlin <sean.tomlin@gmail.com>
2017-07-30 15:32:39 +0100

    Added keyboard input to dancemat.py

--- python/dancemat.py
+++ python/dancemat.py
@@ -24,6 +24,21 @@ class Button:
 
 
 # Object representing dancemat
+class KeyboardInput:
+    self.button_listener = None
+
+    def read(self):
+        for event in pygame.event.get():
+            key = pygame.key.get_pressed()
+            qwerty_input = {'x': key[pygame.K_q],
+                            'up': key[pygame.K_w],
+                            'circle': key[pygame.K_e],
+                            'right': key[pygame.K_d],
+                            'square': key[pygame.K_c],
+                            'down': key[pygame.K_x],
+                            'triangle': key[pygame.K_z],
+                            'left': key[pygame.K_a]}
+
 class DanceMat:
     def __init__(self, number=0):
         self.joystick = pygame.joystick.Joystick(number)
@@ -36,7 +51,20 @@ class DanceMat:
             if self.button_listener is not None:
                 button_dict = {Button.all[n]: self.joystick.get_button(n) == 1 for n in
                                range(0, self.joystick.get_numbuttons())}
+                key = pygame.key.get_pressed()
+                qwerty_input = {'x': key[pygame.K_q],
+                                'up': key[pygame.K_w],
+                                'circle': key[pygame.K_e],
+                                'right': key[pygame.K_d],
+                                'square': key[pygame.K_c],
+                                'down': key[pygame.K_x],
+                                'triangle': key[pygame.K_z],
+                                'left': key[pygame.K_a]}
+                for k in qwerty_input.keys():
+                    if qwerty_input[k]:
+                        button_dict[k] = True
                 self.button_listener(button_dict)
+
 
     # Sets a listener that is alerted to any button press or depress
     def set_button_listener(self, button_listener):
