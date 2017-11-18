c6c877b4ce7df4e16f0c0185b134f763dc7261c7
Richard <richardhayes777@gmail.com>
2017-11-18 12:13:32 +0000

    integration of combinator

--- python/main.py
+++ python/main.py
@@ -4,6 +4,7 @@ from time import sleep
 import pygame
 import signal
 import visual
+import effect
 import sys
 
 filename = 'media/bicycle-ride.mid' if len(sys.argv) == 1 else sys.argv[1]
@@ -17,6 +18,8 @@ clock = pygame.time.Clock()
 
 mat = dancemat.DanceMat(pygame)
 track = player.Track(filename=filename, is_looping=True)
+
+combinator = effect.Combinator("configurations/effects_1.json", track)
 
 play = True
 
@@ -41,24 +44,8 @@ channels[0].message_send_listener = display.on_message_received
 
 # Function to listen for changes to button state
 def listener(status_dict):
-    if status_dict[dancemat.Button.up]:
-        # channels[1].instrument_type += 1
-        track.tempo_shift = 2
-    if status_dict[dancemat.Button.down]:
-        track.tempo_shift = 0.5
-    if status_dict[dancemat.Button.right]:
-        channels[1].instrument_version += 1
-    if status_dict[dancemat.Button.left]:
-        channels[1].instrument_version -= 1
-
-    if status_dict[dancemat.Button.triangle]:
-        channels[0].intervals = player.Intervals([1])
-    if status_dict[dancemat.Button.circle]:
-        channels[0].intervals = player.Intervals([2])
-    if status_dict[dancemat.Button.x]:
-        channels[0].intervals = player.Intervals([3])
-    if status_dict[dancemat.Button.square]:
-        channels[0].intervals = player.Intervals([4])
+    on_buttons = [button for (button, is_on) in status_dict.iteritems() if is_on]
+    combinator.apply_for_buttons(on_buttons)
 
 
 # Attach that listener function to the dancemat
