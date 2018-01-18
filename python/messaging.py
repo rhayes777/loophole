32dffa54a9b63d9124729160986e3e99e4247da3
Richard <richardhayes777@gmail.com>
2018-01-18 21:07:31 +0000

    finally clever messaging works

--- python/messaging.py
+++ python/messaging.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 import mido
+import json
 
 
 class ProcessMessage(object):
@@ -9,32 +10,41 @@ class ProcessMessage(object):
         sys.stdout.flush()
 
     def __repr__(self):
-        return self.__class__.__name__
+        return self.__str__()
+
+    def __str__(self):
+        return "{},{}".format(self.__class__.__name__,
+                              json.dumps({key: str(value) for key, value in self.__dict__.iteritems()}))
 
     @classmethod
     def from_string(cls, as_string):
         as_array = as_string.split(",")
         class_name = as_array[0]
-        return globals()[class_name](*as_array[1:])
+        return globals()[class_name](**json.loads(as_array[1]))
 
 
 class MidiMessage(ProcessMessage, object):
     def __init__(self, mido_message):
-        if isinstance(mido_message, str):
+        if isinstance(mido_message, str) or isinstance(mido_message, unicode):
             self.mido_message = mido.Message.from_str(mido_message)
         else:
             self.mido_message = mido_message
 
-    def __repr__(self):
-        return "{},{}".format(super(MidiMessage, self).__repr__(), self.mido_message)
+    # def __repr__(self):
+    #     return "{},{}".format(super(MidiMessage, self).__repr__(), self.mido_message)
 
     def __eq__(self, other):
         return self.mido_message == other.mido_message
 
 
+class ButtonPress(ProcessMessage, object):
+    def __init__(self, button):
+        self.button = button
+
+
 class MidiTestCase(unittest.TestCase):
     def setUp(self):
-        self.as_string = "MidiMessage,note_on channel=0 note=0 velocity=64 time=0"
+        self.as_string = 'MidiMessage,{"mido_message": "note_on channel=0 note=0 velocity=64 time=0"}'
         self.as_message = MidiMessage(mido.Message(type="note_on"))
 
     def test_midi_to_string(self):
