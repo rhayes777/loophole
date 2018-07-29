3c0e3334697b449e19c27351b03cef3bfc877f0c
Richard Hayes <richardhayes777@gmail.com>
2018-07-29 14:35:04 +0100

    applying an operation to objects in the stream

--- src/iterators.py
+++ src/iterators.py
@@ -1,15 +1,24 @@
+from Queue import Queue
+
+
 class Iterator(object):
-    def __init__(self, source):
+    def __init__(self, source, operation=lambda message: [message]):
         if isinstance(source, list):
             self.source = iter(source).__iter__()
         else:
             self.source = source
+        self.operation = operation
+        self.queue = Queue()
 
     def __iter__(self):
         return self
 
     def next(self):
-        return self.source.next()
+        if not self.queue.empty():
+            return self.queue.get()
+        for message in self.operation(self.source.next()):
+            self.queue.put(message)
+        return self.next()
 
 
 class TestCase(object):
@@ -18,7 +27,12 @@ class TestCase(object):
 
         assert [1, 2, 3] == [n for n in iterator]
 
-    def test_doubleiter(self):
+    def test_double_iter(self):
         iterator = Iterator(Iterator([1, 2, 3]))
 
         assert [1, 2, 3] == [n for n in iterator]
+
+    def test_apply(self):
+        iterator = Iterator([1, 2, 3], operation=lambda x: [2 * x])
+
+        assert [2, 4, 6] == [n for n in iterator]
