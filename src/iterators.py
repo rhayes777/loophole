550437ae88090514738e85a72a9fbb7abc869d41
Richard Hayes <richardhayes777@gmail.com>
2018-07-29 14:27:32 +0100

    passing iterators to iterators

--- src/iterators.py
+++ src/iterators.py
@@ -2,6 +2,8 @@ class Iterator(object):
     def __init__(self, source):
         if isinstance(source, list):
             self.source = iter(source).__iter__()
+        else:
+            self.source = source
 
     def __iter__(self):
         return self
@@ -13,5 +15,10 @@ class Iterator(object):
 class TestCase(object):
     def test_iter(self):
         iterator = Iterator([1, 2, 3])
+
+        assert [1, 2, 3] == [n for n in iterator]
+
+    def test_doubleiter(self):
+        iterator = Iterator(Iterator([1, 2, 3]))
 
         assert [1, 2, 3] == [n for n in iterator]
