61f695787bf7618922b99d21c96a3a8484676cd0
Richard Hayes <richardhayes777@gmail.com>
2018-09-05 19:55:02 +0100

    moving note sprite

--- src/model.py
+++ src/model.py
@@ -4,14 +4,30 @@ class Note(object):
         self.velocity = velocity
 
     def move(self):
-        pass
+        self.position = tuple(position + velocity for position, velocity in zip(self.position, self.velocity))
 
 
 class TestNote(object):
     def test_no_movement(self):
-        note = Note(position=(0, 0), velocity=(1, 0))
-
-        assert note.position == (0, 0)
+        note = Note(position=(0, 0), velocity=(0, 0))
         note.move()
 
         assert note.position == (0, 0)
+
+    def test_up_movement(self):
+        note = Note(position=(0, 0), velocity=(1, 0))
+        note.move()
+
+        assert note.position == (1, 0)
+
+    def test_right_movement(self):
+        note = Note(position=(0, 0), velocity=(0, 1))
+        note.move()
+
+        assert note.position == (0, 1)
+
+    def test_left_movement(self):
+        note = Note(position=(0, 0), velocity=(0, -1))
+        note.move()
+
+        assert note.position == (0, -1)
