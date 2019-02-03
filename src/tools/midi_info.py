2234247489755def8c884a03162c9a32772c1973
Richard Hayes <richardhayes777@gmail.com>
2019-02-03 18:39:28 +0000

    printed channels in use

--- src/tools/midi_info.py
+++ src/tools/midi_info.py
@@ -3,5 +3,17 @@
 import mido
 import sys
 
-f = mido.MidiFile(sys.argv[1])
-f.print_tracks()
+if len(sys.argv) < 2:
+    print("Usage: midi_info.py example.mid")
+    exit(1)
+
+name = sys.argv[1]
+
+f = mido.MidiFile(name)
+
+no_tracks = len(f.tracks)
+
+messages = [message for track in f.tracks for message in track]
+channels = {message.channel for message in messages if hasattr(message, "channel")}
+
+print("{} has {} tracks on channels: {}".format(name, no_tracks, " ".join(map(str, channels))))
