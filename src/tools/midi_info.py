5e1993f3999565c4de68e3ff8562cdf14e6a9380
Richard Hayes <richardhayes777@gmail.com>
2019-03-10 19:11:40 +0000

    better analysis

--- src/tools/midi_info.py
+++ src/tools/midi_info.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-import mido
 import sys
+
+import mido
 
 if len(sys.argv) < 2:
     print("Usage: midi_info.py example.mid")
@@ -14,6 +15,9 @@ f = mido.MidiFile(name)
 no_tracks = len(f.tracks)
 
 messages = [message for track in f.tracks for message in track]
-channels = {message.channel for message in messages if hasattr(message, "channel")}
+
+programs = {message.program + 1 for message in messages if message.type == "program_change"}
+channels = {message.channel + 1 for message in messages if hasattr(message, "channel")}
 
 print("{} has {} tracks on channels: {}".format(name, no_tracks, " ".join(map(str, channels))))
+print("It uses programs numbers {}".format(" ".join(map(str, programs))))
