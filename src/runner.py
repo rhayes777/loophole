ae2b6f8bb7a26d145af2ad65958d4be264910b75
Richard Hayes <richardhayes777@gmail.com>
2019-05-27 10:08:30 +0100

    log number of objects

--- src/runner.py
+++ src/runner.py
@@ -2,11 +2,19 @@ import pygame
 
 import scoreboard
 from space_fighter_game import space_fighter_game
+from visual import font
 from visual import visual
 
 pygame.init()
 pygame.display.init()
 clock = pygame.time.Clock()
+
+
+def print_sprites():
+    print "len(sprite_group_player) == {}".format(len(visual.sprite_group_player.sprites()))
+    print "len(sprite_group_notes) == {}".format(len(visual.sprite_group_notes.sprites()))
+    print "len(sprite_group_energy_glows) == {}".format(len(visual.sprite_group_energy_glows.sprites()))
+    print "len(notices_list) == {}".format(len(font.notices_list))
 
 
 def run_game(game):
@@ -16,6 +24,8 @@ def run_game(game):
         clock.tick(24)
 
         game.step_forward()
+
+        print_sprites()
 
         visual.draw()
         visual.sprite_group_notes.empty()
